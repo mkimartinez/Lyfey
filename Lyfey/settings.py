42c2eebde0de749adeb020af7efbb02e3bd59c11
martinez <martinamakobe@gmail.com>
2018-07-02 16:12:37 +0800

    Update settings.py

--- Lyfey/settings.py
+++ Lyfey/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'postgres',
         'NAME': 'Lyfey-Page',
-        'PASSWORD' : 'MKI2017',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' :'5432'
     },
